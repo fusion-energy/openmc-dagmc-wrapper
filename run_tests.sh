e782fc9960ea54422258458fc32cd51f20735618
Jonathan Shimwell <mail@jshimwell.com>
2021-07-13 10:50:33 +0100

    removed duplicate -- typo

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,7 @@
 
-pytest tests/notebook_testing.py -v --cov=paramak_neutronics --cov-append -- --cov-report term --cov-report xml
-pytest tests/test_NeutronicModel.py -v --cov=paramak_neutronics --cov-append -- --cov-report term --cov-report xml
-pytest tests/test_Reactor_neutronics.py -v --cov=paramak_neutronics --cov-append -- --cov-report term --cov-report xml
-pytest tests/test_Shape_neutronics.py -v --cov=paramak_neutronics --cov-append -- --cov-report term --cov-report xml
-pytest tests/test_example_neutronics_simulations.py -v --cov=paramak_neutronics --cov-append -- --cov-report term --cov-report xml
-pytest tests/test_neutronics_utils.py -v --cov=paramak_neutronics --cov-append -- --cov-report term --cov-report xml
+pytest tests/notebook_testing.py -v --cov=paramak_neutronics --cov-append --cov-report term --cov-report xml
+pytest tests/test_NeutronicModel.py -v --cov=paramak_neutronics --cov-append --cov-report term --cov-report xml
+pytest tests/test_Reactor_neutronics.py -v --cov=paramak_neutronics --cov-append --cov-report term --cov-report xml
+pytest tests/test_Shape_neutronics.py -v --cov=paramak_neutronics --cov-append --cov-report term --cov-report xml
+pytest tests/test_example_neutronics_simulations.py -v --cov=paramak_neutronics --cov-append --cov-report term --cov-report xml
+pytest tests/test_neutronics_utils.py -v --cov=paramak_neutronics --cov-append --cov-report term --cov-report xml
