375e559fcbbd3f4e32e79a78c8194820cb00fcdf
Jonathan Shimwell <mail@jshimwell.com>
2021-09-22 18:13:21 +0100

    updated to new class layout

--- openmc_dagmc_wrapper/__init__.py
+++ openmc_dagmc_wrapper/__init__.py
@@ -1,8 +1,5 @@
-from .utils import process_results
-from .utils import create_initial_particles
-from .utils import extract_points_from_initial_source
-from .utils import silently_remove_file
-from .utils import plotly_trace
-from .utils import diff_between_angles
 
-from .neutronics_model import NeutronicsModel
+from .neutronics_model import Geometry
+from .neutronics_model import Materials
+from .neutronics_model import Tally
+from .neutronics_model import Model
