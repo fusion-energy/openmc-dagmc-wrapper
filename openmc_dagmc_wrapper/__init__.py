217fa6fd326b069fb2ea124302917d879bbdd7be
remdelaportemathurin <rdelaportemathurin@gmail.com>
2021-09-23 12:45:52 +0200

    added all Tally classes to init

--- openmc_dagmc_wrapper/__init__.py
+++ openmc_dagmc_wrapper/__init__.py
@@ -2,5 +2,6 @@ from .utils import process_results
 
 from .Geometry import Geometry
 from .Materials import Materials
-from .Tally import CellTally
+from .Tally import CellTally, CellTallies, MeshTally2D, MeshTallies2D, \
+    MeshTally3D, MeshTallies3D
 from .Model import Model
