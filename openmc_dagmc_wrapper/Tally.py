5bc8e86fd16c0926b0c91362b5f646345254082f
autopep8 <autopep8@users.noreply.github.com>
2021-10-14 09:44:19 +0000

    Automated autopep8 fixes

--- openmc_dagmc_wrapper/Tally.py
+++ openmc_dagmc_wrapper/Tally.py
@@ -162,7 +162,12 @@ class CellTallies:
         h5m_filename
     """
 
-    def __init__(self, tally_types, targets=[None], materials=None, h5m_filename=None):
+    def __init__(
+            self,
+            tally_types,
+            targets=[None],
+            materials=None,
+            h5m_filename=None):
         self.tallies = []
         self.tally_types = tally_types
         self.targets = targets
@@ -179,8 +184,10 @@ class CellTallies:
         for score in self.tally_types:
             for target in all_targets:
                 self.tallies.append(
-                    CellTally(tally_type=score, target=target, materials=materials)
-                )
+                    CellTally(
+                        tally_type=score,
+                        target=target,
+                        materials=materials))
 
 
 class TetMeshTally(Tally):
@@ -232,7 +239,10 @@ class TetMeshTallies:
         self.tally_types = tally_types
         for score in self.tally_types:
             for filename in filenames:
-                self.tallies.append(TetMeshTally(tally_type=score, filename=filename))
+                self.tallies.append(
+                    TetMeshTally(
+                        tally_type=score,
+                        filename=filename))
 
 
 class MeshTally3D(Tally):
@@ -494,7 +504,9 @@ def compute_filters(tally_type):
         energy_function_filter_n = openmc.EnergyFunctionFilter(
             energy_bins_n, dose_coeffs_n
         )
-        additional_filters = [neutron_particle_filter, energy_function_filter_n]
+        additional_filters = [
+            neutron_particle_filter,
+            energy_function_filter_n]
     elif tally_type == "photon_effective_dose":
         energy_function_filter_p = openmc.EnergyFunctionFilter(
             energy_bins_p, dose_coeffs_p
