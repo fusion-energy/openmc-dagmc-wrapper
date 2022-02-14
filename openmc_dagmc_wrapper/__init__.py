a3a09285948eae448e87cdad5d029878b3a2bba1
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-02-14 23:51:42 +0000

    test commit

--- openmc_dagmc_wrapper/__init__.py
+++ openmc_dagmc_wrapper/__init__.py
@@ -2,7 +2,8 @@ from .utils import create_material, diff_between_angles
 
 from .Geometry import Geometry
 from .Materials import Materials
-from .Tally import Tally
 
+from .Tally import Tally
 from .tallies import *
+
 from .Settings import FusionSettings
