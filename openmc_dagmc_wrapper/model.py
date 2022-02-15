321ad3b16ec4bd6a55d1367c3a532624021075ed
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-02-15 14:25:54 +0000

    added docstrings

--- openmc_dagmc_wrapper/model.py
+++ openmc_dagmc_wrapper/model.py
@@ -12,11 +12,27 @@ class Model(openmc.Model):
         tallies=None,
         plots=None
     ):
+        """Inits Model
+
+        Args:
+            geometry (odw.Geometry, optional): Geometry information. Defaults
+                to None.
+            materials (odw.Materials, optional): Materials information.
+                Defaults to None.
+            settings (openmc.Settings, optional): Settings information.
+                Defaults to None.
+            tallies (openmc.Tallies, optional): Tallies information.
+                Defaults to None.
+            plots (openmc.Plots, optional): Plot information. Defaults to None.
+        """
         super().__init__(geometry, materials, settings, tallies, plots)
         self.materials.checks(self.geometry.h5m_filename)
         self.check_tallies_meshes_corners()
 
     def check_tallies_meshes_corners(self):
+        """Iterates through tallies and check if they have a RegularMesh2D.
+        If the RegularMesh2D doesn't have corners, add them from the geometry.
+        """
         for tally in self.tallies:
             for filter in tally.filters:
                 if isinstance(filter, openmc.MeshFilter):
