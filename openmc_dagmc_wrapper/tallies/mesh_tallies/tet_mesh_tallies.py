42d3cf4925b841222dc7bc63a4fc428a3f464152
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-02-15 02:30:28 +0000

    refactored TetMeshTally

--- openmc_dagmc_wrapper/tallies/mesh_tallies/tet_mesh_tallies.py
+++ openmc_dagmc_wrapper/tallies/mesh_tallies/tet_mesh_tallies.py
@@ -1,9 +1,9 @@
 import openmc
 
-from openmc_dagmc_wrapper import Tally
+from . import MeshTally
 
 
-class TetMeshTally(Tally):
+class TetMeshTally(MeshTally):
     """Usage:
     my_tally = odw.TetMeshTally(tally_type='TBR', filename="file.h5m")
     my_tally2 = odw.TetMeshTally(tally_type='TBR', filename="file.exo")
@@ -14,26 +14,23 @@ class TetMeshTally(Tally):
     """
 
     def __init__(self, tally_type, filename, **kwargs):
-        self.filename = filename
-        self.tally_type = tally_type
-        super().__init__(tally_type, **kwargs)
 
-        self.create_unstructured_mesh()
-        self.filters.append(openmc.MeshFilter(self.umesh))
+        mesh = self.create_mesh(filename)
+        super().__init__(tally_type, mesh, **kwargs)
         self.name = tally_type + "_on_3D_u_mesh"
 
-    def create_unstructured_mesh(self):
-        if self.filename.endswith(".exo"):
+    def create_mesh(self, filename):
+        if filename.endswith(".exo"):
             # requires a exo file export from cubit
             library = "libmesh"
-        elif self.filename.endswith(".h5m"):
+        elif filename.endswith(".h5m"):
             # requires a .cub file export from cubit and mbconvert to h5m
             # format
             library = "moab"
         else:
             msg = "only h5m or exo files are accepted as valid " "filename values"
             raise ValueError(msg)
-        self.umesh = openmc.UnstructuredMesh(self.filename, library=library)
+        return openmc.UnstructuredMesh(filename, library=library)
 
 
 class TetMeshTallies:
